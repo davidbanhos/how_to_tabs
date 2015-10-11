c69c2e401fb190686ab8467e9ffee7764cd2a4eb
David Saggese Banhos <david.banhos@gmail.com>
2015-10-11 15:08:38 +0800

    Now build script checks node version

--- jakefile.js
+++ jakefile.js
@@ -5,9 +5,23 @@
 (function() {
     "use strick";
 
+    var EXPECTED_NODE_VERSION = "v0.12.4";
+
     desc("Default build");
-    task("default", function() {
+    task("default", [ "version" ] , function() {
         console.log("\n\nBUILD OK");
+    });
+
+    desc("Check Node version");
+    task("version", function() {
+        console.log("Checking node version: .");
+
+        var actualVersion = process.version;
+        //console.log(actualVersion);
+
+        if (actualVersion !== EXPECTED_NODE_VERSION) {
+            fail("Incorrect Node version, expected " + EXPECTED_NODE_VERSION + ", but was " + actualVersion);
+        }
     });
 
 }());
