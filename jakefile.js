a8371b63538d7d8ae69140c266f5852dfdeb3307
David Saggese Banhos <david.banhos@gmail.com>
2015-10-11 15:18:55 +0800

    Checking node version from package.json

--- jakefile.js
+++ jakefile.js
@@ -5,23 +5,22 @@
 (function() {
     "use strick";
 
-    var EXPECTED_NODE_VERSION = "v0.12.4";
-
     desc("Default build");
+
     task("default", [ "version" ] , function() {
         console.log("\n\nBUILD OK");
     });
-
     desc("Check Node version");
+
     task("version", function() {
         console.log("Checking node version: .");
-
         var actualVersion = process.version;
-        //console.log(actualVersion);
 
-        if (actualVersion !== EXPECTED_NODE_VERSION) {
-            fail("Incorrect Node version, expected " + EXPECTED_NODE_VERSION + ", but was " + actualVersion);
+        var packageJson = require("./package.json");
+        var expectedVersion = "v" + packageJson.engines.node;
+
+        if (actualVersion !== expectedVersion) {
+            fail("Incorrect Node version, expected " + expectedVersion + ", but was " + actualVersion);
         }
     });
-
 }());
